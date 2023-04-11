96e7f8de186fd87ad1c65ffbd4f2e4aeded1173b
olapietka <ola.pietka@webinterpret.com>
2023-04-11 13:00:50 +0200

    Create baseKeyFactory interface

--- src/data_snack/key_factory.py
+++ src/data_snack/key_factory.py
@@ -1,14 +1,44 @@
-from typing import Protocol, Text, Any
+from abc import ABC
+from typing import Text
 
 
-class KeyFactory(Protocol):
-    def __call__(self, type_name: Text, *key_values: Text) -> Text:
+class BaseKeyFactory(ABC):
+    """
+    KeyFactory is used to get key strings and their pattern
+    """
+
+    def get_key(self, type_name: Text, *key_values: Text) -> Text:
+        """
+        Gets key string.
+
+        :param type_name: type name
+        :param key_values: key values
+        :return: key string in specified format
+        """
+        ...
+
+    def get_pattern(self, type_name: Text) -> Text:
+        """
+        Gets pattern string.
+
+        :param type_name: type name
+        :return: pattern string in specified format
+        """
         ...
 
 
-def key_factory(type_name: Text, *key_values: Any) -> Text:
-    return f"{type_name}-{'_'.join(map(str, key_values))}"
+class SingleKeyFactory(BaseKeyFactory):
+    def get_key(self, type_name: Text, *key_values: Text) -> Text:
+
+        return f"{type_name}-{'_'.join(map(str, key_values))}"
+
+    def get_pattern(self, type_name: Text) -> Text:
+        return f"{type_name}-*"
 
 
-def key_factory_cluster(type_name: Text, *key_values: Any) -> Text:
-    return f"{{{type_name}}}-{'_'.join(map(str, key_values))}"
+class ClusterKeyFactory(BaseKeyFactory):
+    def get_key(self, type_name: Text, *key_values: Text) -> Text:
+        return f"{{{type_name}}}-{'_'.join(map(str, key_values))}"
+
+    def get_pattern(self, type_name: Text) -> Text:
+        return f"{{{type_name}}}-*"
